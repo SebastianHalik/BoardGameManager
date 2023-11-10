7720e09fe060bca4bb1a10210a59baa2ee387110
Sebastian Halik <sebastianhalik1@gmail.com>
2023-11-10 12:52:14 +0100

    Initial commit

---
+++ src/main/java/boardgame/manager/demo/BoardGameManagerApp.java
@@ -0,0 +1,13 @@
+package boardgame.manager.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameManagerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameManagerApp.class, args);
+	}
+
+}
---
+++ src/test/java/boardgame/manager/demo/BoardGameManagerAppTests.java
@@ -0,0 +1,13 @@
+package boardgame.manager.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameManagerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
